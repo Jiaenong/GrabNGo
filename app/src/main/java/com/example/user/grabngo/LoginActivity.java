32f93bea4904f867d66e1696f0db11c261fe42b3
Jiaenong <ongje-pa15@student.tarc.edu.my>
2018-10-07 23:39:57 +0800

    Fouth commit

--- app/src/main/java/com/example/user/grabngo/LoginActivity.java
+++ app/src/main/java/com/example/user/grabngo/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.grabngo;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
@@ -13,11 +14,21 @@ import android.widget.EditText;
 
 import com.example.user.grabngo.Admin.ManagerHomeActivity;
 import com.example.user.grabngo.Admin.StaffHomeActivity;
+import com.example.user.grabngo.Class.Customer;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button btnSignUp, btnLogin;
     private EditText editTextEmail, editTextPassword;
+    private ProgressDialog pDialog;
+
+    private FirebaseFirestore mFirebaseFirestore;
+    private CollectionReference mCollectionReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,10 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin = (Button)findViewById(R.id.btn_login);
         editTextEmail = (EditText)findViewById(R.id.editText_email);
         editTextPassword = (EditText)findViewById(R.id.editText_password);
+        pDialog = new ProgressDialog(this);
+
+        mFirebaseFirestore = FirebaseFirestore.getInstance();
+        mCollectionReference = mFirebaseFirestore.collection("Customer");
 
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,20 +60,40 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(LoginActivity.this, R.style.AlertDialogCustom));
-                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                if(!pDialog.isShowing())
+                    pDialog.setMessage("Signing in...");
+                pDialog.show();
+                mCollectionReference.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        return;
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        for(QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots)
+                        {
+                            Customer customer = documentSnapshot.toObject(Customer.class);
+                            String email = editTextEmail.getText().toString();
+                            String password = editTextPassword.getText().toString();
+                            if(email.equals(customer.getEmail()) && password.equals(customer.getPassword()))
+                            {
+                                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                                finish();
+                                if(pDialog.isShowing())
+                                    pDialog.dismiss();
+                            }
+                        }
                     }
                 });
-                builder.setTitle("Error");
-                builder.setMessage("Email address and password are required to proceed");
-                AlertDialog alert = builder.create();
-
 
                 if(editTextEmail.getText().toString().equals("")||editTextPassword.getText().toString().equals("")){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(LoginActivity.this, R.style.AlertDialogCustom));
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            return;
+                        }
+                    });
+                    builder.setTitle("Error");
+                    builder.setMessage("Email address and password are required to proceed");
+                    AlertDialog alert = builder.create();
                     alert.show();
                     return;
                 }else{
@@ -74,10 +109,6 @@ public class LoginActivity extends AppCompatActivity {
                         return;
                     }
                 }
-
-                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                startActivity(intent);
-                finish();
             }
         });
     }
--- app/src/main/java/com/example/user/grabngo/SignUpActivity.java
+++ app/src/main/java/com/example/user/grabngo/SignUpActivity.java
@@ -8,15 +8,60 @@ import android.os.Bundle;
 import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+
+import com.example.user.grabngo.Class.Customer;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class SignUpActivity extends AppCompatActivity {
     private ImageView btn_back;
     private Button btnSignup;
+    private EditText editTextEmailAddress, editTextPassword, editTextRetypePassword, editTextCustomerName, editTextAddress;
+    private RadioButton radioButtonMale, radioButtonFemale;
+    private FirebaseFirestore mFirebaseFirestore;
+    private DocumentReference mDocumentReference;
+    private CollectionReference mCollectionReference;
+
+    private String autoID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        editTextEmailAddress = (EditText)findViewById(R.id.editTextEmailAddress);
+        editTextPassword = (EditText)findViewById(R.id.editTextPassword);
+        editTextRetypePassword = (EditText)findViewById(R.id.editTextRetypePassword);
+        editTextCustomerName = (EditText)findViewById(R.id.editTextCustomerName);
+        editTextAddress = (EditText)findViewById(R.id.editTextAddress);
+        radioButtonMale = (RadioButton)findViewById(R.id.radioButtonMale);
+        radioButtonFemale = (RadioButton)findViewById(R.id.radioButtonFemale);
+
+        mFirebaseFirestore = FirebaseFirestore.getInstance();
+        mCollectionReference = mFirebaseFirestore.collection("Customer");
+        mCollectionReference.orderBy("email",Query.Direction.DESCENDING).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                if(queryDocumentSnapshots.isEmpty()){
+                     autoID = "C0001";
+                }else{
+                    for(QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots)
+                    {
+                        String key = documentSnapshot.getId();
+                        int num = Integer.parseInt(key.substring(3));
+                        num++;
+                        autoID = "C000"+num;
+                    }
+                }
+            }
+        });
 
         btn_back = (ImageView)findViewById(R.id.btn_back);
 
@@ -31,6 +76,49 @@ public class SignUpActivity extends AppCompatActivity {
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String password = editTextPassword.getText().toString();
+                String retypepassword = editTextRetypePassword.getText().toString();
+                if(!(retypepassword.equals(password)))
+                {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(SignUpActivity.this, R.style.AlertDialogCustom));
+                    builder.setTitle("Sign Up Fail");
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            editTextEmailAddress.setText("");
+                            editTextPassword.setText("");
+                            editTextRetypePassword.setText("");
+                            editTextAddress.setText("");
+                            editTextCustomerName.setText("");
+                            return;
+                        }
+                    });
+                    builder.setMessage("Invalid retype password.");
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                }else {
+                    SaveCustomer();
+                }
+            }
+        });
+    }
+
+    private void SaveCustomer()
+    {
+        mDocumentReference = mFirebaseFirestore.document("Customer/"+autoID);
+        String email = editTextEmailAddress.getText().toString();
+        String password = editTextPassword.getText().toString();
+        String name = editTextCustomerName.getText().toString();
+        String address = editTextAddress.getText().toString();
+        String gender;
+        if(radioButtonMale.isChecked()){
+            gender = radioButtonMale.getText().toString();
+        }else
+            gender = radioButtonFemale.getText().toString();
+        Customer customer = new Customer(email, password, name, address, gender);
+        mDocumentReference.set(customer).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(SignUpActivity.this, R.style.AlertDialogCustom));
                 builder.setTitle("Sign Up Success");
                 builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
@@ -46,7 +134,5 @@ public class SignUpActivity extends AppCompatActivity {
                 alert.show();
             }
         });
-
-
     }
 }
